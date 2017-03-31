e81beecdac8495c21c7a0f3ce98b699ac77cdeee
miguelms95 <miguelms.ovi@gmail.com>
2017-03-31 16:45:30 +0200

    check valid urls

--- scan_urls_web.py
+++ scan_urls_web.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 from BeautifulSoup import BeautifulSoup
 import requests
-import time
 
 contadorSaltos = 0
 urlsVisitadas = []
 
 def extraerdatos(url):
     global contadorSaltos
-    page = requests.get(url);
-    if (page.status_code == 200):   # la página web ha cargado correctamente
-        contadorSaltos +=1;
-        html = BeautifulSoup(page.content.decode('utf-8', 'ignore'))
-        print ' # ' + str(contadorSaltos) + ' - ' + html.find('title').string
-        print html.findAll('a')[2]
-        urls_pagina = html.findAll('a')
+    if url not in urlsVisitadas:
+        page = requests.get(url);
+        if (page.status_code == 200):   # la página web ha cargado correctamente
+            contadorSaltos +=1;
+            html = BeautifulSoup(page.content.decode('utf-8', 'ignore'))
+            print ' # ' + str(contadorSaltos) + ' - ' + html.find('title').string
+            urls_pagina = html.findAll('a')
+            for url in urls_pagina:
+                url = url.get('href').encode('utf-8')
+                if(url != '#' and ('http' or 'https') in url):
+                    print 'url: ' + str(url)
+                    urlsVisitadas.append(str(url))
+                    extraerdatos(str(url))
+        else:
+            print 'Error en la web' + url
 
-        time.sleep(1)
 
 extraerdatos('http://miguelms.es')
-time.sleep(1)
-
+print 'number of links: ' + str(len(urlsVisitadas))
+print contadorSaltos
