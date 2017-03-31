88d80f6fefcf33967f41ee568e187df22e04c9ff
miguelms95 <miguelms.ovi@gmail.com>
2017-03-31 18:25:33 +0200

    some improvements

--- scan_urls_web.py
+++ scan_urls_web.py
@@ -13,19 +13,32 @@ def extraerdatos(url):
         if (page.status_code == 200):   # la página web ha cargado correctamente
             contadorSaltos +=1;
             urlsVisitadas.append(url)
-            html = BeautifulSoup(page.content.decode('utf-8', 'ignore'))
-            print ' # ' + str(contadorSaltos) + ' - ' + html.find('title').string
-            urls_pagina = html.findAll('a')
-            for url in urls_pagina:
-                url = url.get('href').encode('utf-8')
-                if (url != None):  # There are <a> items without 'href' attribute
-                    if(url not in listaUrls):
-                        if(url != '#' and ('http' or 'https') in url):
-                            print 'url: ' + str(url)
-                            listaUrls.append(str(url))
-                            extraerdatos(str(url))
+            try:
+                html = BeautifulSoup(page.content.decode('utf-8', 'ignore'))
+                titulo = html.find('title')
+                if(titulo != None):
+                    titulo=titulo.string
+                    print ' # ' + str(contadorSaltos) + ' - ' + titulo
+                else:
+                    print ' # ' + str(contadorSaltos)
+                urls_pag_raw = html.findAll('a')
+                urls_pag = []
+                for url in urls_pag_raw:
+                    url = url.get('href')
+                    if(url != None):
+                        urls_pag.append(url)
+                for url in urls_pag:
+                    url = url.encode('utf-8')
+                    if (url != None):  # There are <a> items without 'href' attribute
+                        if(url not in listaUrls):
+                            if(url != '#' and ('http' or 'https') in url):
+                                print 'url: ' + str(url)
+                                listaUrls.append(str(url))
+                                extraerdatos(str(url))
+            except UnicodeEncodeError:
+                print 'error unicode'
         else:
-            print 'Error en la web' + url +'\n'
+            print 'Error en la web ' + url +'\n'
 
 
 extraerdatos('http://miguelms.es')
