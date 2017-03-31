9b6db72f378d470ac2d3ecf2c968eef237056897
miguelms95 <miguelms.ovi@gmail.com>
2017-03-31 16:54:59 +0200

    v1.2

--- scan_urls_web.py
+++ scan_urls_web.py
@@ -4,6 +4,7 @@ import requests
 
 contadorSaltos = 0
 urlsVisitadas = []
+listaUrls = []
 
 def extraerdatos(url):
     global contadorSaltos
@@ -11,18 +12,20 @@ def extraerdatos(url):
         page = requests.get(url);
         if (page.status_code == 200):   # la página web ha cargado correctamente
             contadorSaltos +=1;
+            urlsVisitadas.append(url)
             html = BeautifulSoup(page.content.decode('utf-8', 'ignore'))
             print ' # ' + str(contadorSaltos) + ' - ' + html.find('title').string
             urls_pagina = html.findAll('a')
             for url in urls_pagina:
                 url = url.get('href').encode('utf-8')
-                if(url not in urlsVisitadas):
-                    if(url != '#' and ('http' or 'https') in url):
-                        print 'url: ' + str(url)
-                        urlsVisitadas.append(str(url))
-                        extraerdatos(str(url))
+                if (url != None):  # There are <a> items without 'href' attribute
+                    if(url not in listaUrls):
+                        if(url != '#' and ('http' or 'https') in url):
+                            print 'url: ' + str(url)
+                            listaUrls.append(str(url))
+                            extraerdatos(str(url))
         else:
-            print 'Error en la web' + url
+            print 'Error en la web' + url +'\n'
 
 
 extraerdatos('http://miguelms.es')
